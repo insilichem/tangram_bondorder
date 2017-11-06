5da0241a6ec5e4cf200dcb4a1a69260e05b1d56a
Jaime RGP <jaime.rogue@gmail.com>
2017-11-06 13:16:09 +0100

    Add version check url

--- bondorder/gui.py
+++ bondorder/gui.py
@@ -9,9 +9,10 @@ import Pmw
 # Chimera stuff
 import chimera
 # Own
-from libplume.ui import PlumeBaseDialog
-from chimera.widgets import MoleculeOptionMenu
+from libplume.ui import PlumeBaseDialog, STYLES
+from chimera.widgets import MoleculeOptionMenu, SortableTable
 from bondorder.core import assign_bond_orders, draw_bond_orders
+
 
 ui = None  # singleton
 def showUI():
@@ -27,8 +28,9 @@ class BondOrderDialog(PlumeBaseDialog):
 
     buttons = ('Draw', 'Close')
     default = None
-    help = 'http://www.insilichem.com'
-
+    help = "https://github.com/insilichem/plume_bondorder"
+    VERSION = '0.0.1'
+    VERSION_URL = "https://api.github.com/repos/insilichem/plume_bondorder/releases/latest"
 
     def __init__(self, *args, **kwargs):
         # GUI init
@@ -39,7 +41,7 @@ class BondOrderDialog(PlumeBaseDialog):
         super(BondOrderDialog, self).__init__(resizable=False, *args, **kwargs)
 
         # Triggers
-        chimera.triggers.addHandler('selection changed', self._cb_fill_order, None)
+        chimera.triggers.addHandler('selection changed', self._cmd_hilite_selected, None)
 
     def fill_in_ui(self, parent):
         self.canvas.columnconfigure(0, weight=1)
@@ -61,18 +63,24 @@ class BondOrderDialog(PlumeBaseDialog):
 
         row +=1
         self.ui_edition = tk.LabelFrame(self.canvas, text='Manual edition')
-        self.ui_order_fld = Pmw.EntryField(self.ui_edition, entry_width=4,
-                                           validate=self._order_validator,
-                                           labelpos='w', label_text='<Select one bond>')
-        self.ui_order_fld['entry_state'] = 'disabled'
-        self.ui_order_btn = tk.Button(self.ui_edition, text='Write',
-                                      command=self._cmd_order_btn)
-        self.ui_order_btn['state'] = 'disabled'
-        self.ui_edition.grid(row=row, padx=5, pady=5, sticky='we')
-        self.ui_edition.columnconfigure(0, weight=1)
-        self.ui_order_fld.grid(row=0, column=0, padx=5, pady=5, sticky='we')
-        self.ui_order_btn.grid(row=0, column=1, padx=5, pady=5)
+        self.ui_fill_all_btn = tk.Button(self.ui_edition, text='Fill with all')
+        self.ui_fill_selected_btn = tk.Button(self.ui_edition, text='Fill with selection')
+        self.ui_fill_defined_btn = tk.Button(self.ui_edition, text='Fill with defined')
+        self.ui_hilite_selected_btn = tk.Button(self.ui_edition, text='Highlight selected')
+        buttons = (self.ui_fill_all_btn, self.ui_fill_selected_btn,
+                   self.ui_fill_defined_btn, self.ui_hilite_selected_btn)
+        self.auto_pack(self.ui_edition, buttons, padx=5, pady=5, side='left')
+        self.ui_edition.grid(row=row, columnspan=3, padx=5, pady=5, sticky='we')
 
+        row +=1
+        self.ui_table = t = _SortableTableWithEntries(self.canvas)
+        self.ui_table.grid(row=row, padx=5, pady=5, sticky='news')
+        kw = dict(anchor='w', refresh=False)
+        t.addColumn('Bond', 'bond', format=str, headerPadX=75, **kw)
+        t.addColumn('Order', 'var_order', format=lambda a: a, headerPadX=5, **kw)
+        t.setData([_BondTableProxy(bond=b) for b in chimera.selection.currentBonds()])
+        t.grid(row=row, column=0, columnspan=3, padx=5, pady=5, sticky='news')
+        self.canvas.rowconfigure(row, weight=1)
 
     def Draw(self):
         m = self.ui_molecule.getvalue()
@@ -82,11 +90,6 @@ class BondOrderDialog(PlumeBaseDialog):
         global ui
         ui = None
         super(BondOrderDialog, self).Close()
-
-    def _cmd_order_btn(self, *args):
-        order = self.ui_order_fld.getvalue()
-        for bond in chimera.selection.currentBonds():
-            bond.order = order
 
     def _cmd_calculate_btn(self, *args):
         molecule = self.ui_molecule.getvalue()
@@ -98,39 +101,64 @@ class BondOrderDialog(PlumeBaseDialog):
         else:
             self.Draw()
 
-    def _cb_fill_order(self, *args, **kwargs):
-        selected_bonds = chimera.selection.currentBonds()
-        length = len(selected_bonds)
-        if not length:
-            label = '<Select one bond>'
-            self.ui_order_fld.clear()
-            self.ui_order_fld['entry_state'] = 'disabled'
-            self.ui_order_fld['label_text'] = label
+    def _cmd_fill_all(self, *args):
+        pass
 
-            self.ui_order_btn['state'] = 'disabled'
-        elif length == 1:
-            bond = selected_bonds[0]
-            label = str(bond)
-            self.ui_order_fld['entry_state'] = 'normal'
-            self.ui_order_fld['label_text'] = label
-            self.ui_order_btn['state'] = 'normal'
-            order = getattr(bond, 'order', '')
-            self.ui_order_fld.setvalue(order)
-        else:
-            self.ui_order_fld['entry_state'] = 'normal'
-            self.ui_order_fld['label_text'] = '<More than one selected>'
-            self.ui_order_fld.setvalue('')
-            self.ui_order_btn['state'] = 'normal'
+    def _cmd_fill_selected(self, *args):
+        pass
 
-    def _order_validator(self, value):
-        try:
-            value = float(value)
-        except (TypeError, ValueError):
-            value = -1
-        btn = getattr(self, 'ui_order_btn', {})
+    def _cmd_fill_defined(self, *args):
+        pass
+
+    def _cmd_hilite_selected(self, *args):
+        pass
+
+
+
+class _BondTableProxy(object):
+
+    """
+    Proxy object to ease the creation of table rows
+
+    Attributes
+    ----------
+    bond=bond
+    order=order
+    """
+
+    def __init__(self, *args, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+        self.var_order = tk.DoubleVar()
+
+    @property
+    def order(self):
+        return self.var_order.get()
+
+    @order.setter
+    def order(self, value):
+        self.var_order.set(round(value, 1))
+
+
+class _SortableTableWithEntries(SortableTable):
+
+    def _createCell(self, hlist, row, col, datum, column):
+        contents = column.displayValue(datum)
+        if isinstance(contents, tk.StringVar):
+            entry = Pmw.EntryField(hlist,
+                                   entry_textvariable=contents,
+                                   entry_width=3,
+                                   validate=self._validate,
+                                   **STYLES[Pmw.EntryField])
+            widget = self._widgetData[(datum, column)] = entry
+            hlist.item_create(row, col, itemtype="window", window=entry)
+            return
+
+        SortableTable._createCell(self, hlist, row, col, datum, column)
+
+    @staticmethod
+    def _validate(value):
         if 0 < value < 3:
-            btn['state'] = 'normal'
             return Pmw.OK
-        else:
-            btn['state'] = 'disabled'
-            return Pmw.PARTIAL
+        return Pmw.PARTIAL
