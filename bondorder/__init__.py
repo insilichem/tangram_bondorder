b8b29532d8620c0e419bdde728e7b682c34ddfb7
Jaime RGP <jaime.rogue@gmail.com>
2019-02-01 17:00:22 +0100

    use versioneer

--- bondorder/__init__.py
+++ bondorder/__init__.py
@@ -5,3 +5,6 @@
 Implements several bond order perception engines within UCSF Chimera
 and allows their representation.
 """
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
